080fa4a4182247f004344bad1ac156c4f31242fb
Mohammad <mmkhajah@pop-os.localdomain>
2020-08-04 04:02:57 +0300

    complete smf generalization run

--- smf_generalization_pipeline.py
+++ smf_generalization_pipeline.py
@@ -16,34 +16,9 @@ import utils.eval_funcs
 import analysis.fig_cv_performance
 
 n_models = 10
-go_postfix = "unified_sgo"
-output_dir = "../results/smf_generation_unified_sgo"
-
-
+go_postfix = "common_sgo"
+output_dir = "../results/smf_generation"
 splits = [(i, 0) for i in range(n_models)]
-
-# # 1. unify sGO terms
-# unified_sgo_terms = utils.union_sgo_terms.get_union([
-#     "../generated-data/features/ppc_yeast-sgo.npz", 
-#     "../generated-data/features/ppc_pombe_sgo.npz",
-#     "../generated-data/features/ppc_human_sgo.npz",
-#     "../generated-data/features/ppc_dro_sgo.npz"
-# ])
-# with open("../tmp/unified_sgo_terms", "w") as f:
-#     f.write("\n".join(unified_sgo_terms))
-
-# subprocess.call(shlex.split("../tools/owltools ../tools/go.obo --gaf ../data-sources/dro/fb.gaf --map2slim --idfile ../tmp/unified_sgo_terms --write-gaf ../tmp/dro.unified.sgo.gaf"))
-# feature_preprocessing.sgo.main("../generated-data/ppc_dro", "../tmp/dro.unified.sgo.gaf", 1, output_path="../generated-data/features/ppc_dro_unified_sgo", all_go_terms=unified_sgo_terms)
-
-# subprocess.call(shlex.split("../tools/owltools ../tools/go.obo --gaf ../data-sources/pombe/pombase.gaf --map2slim --idfile ../tmp/unified_sgo_terms --write-gaf ../tmp/pombase.unified.sgo.gaf"))
-# feature_preprocessing.sgo.main("../generated-data/ppc_pombe", "../tmp/pombase.unified.sgo.gaf", 1, output_path="../generated-data/features/ppc_pombe_unified_sgo", all_go_terms=unified_sgo_terms)
-
-# subprocess.call(shlex.split("../tools/owltools ../tools/go.obo --gaf ../data-sources/human/goa_human.gaf --map2slim --idfile ../tmp/unified_sgo_terms --write-gaf ../tmp/human.unified.sgo.gaf"))
-# feature_preprocessing.sgo.main("../generated-data/ppc_human", "../tmp/human.unified.sgo.gaf", 2, output_path="../generated-data/features/ppc_human_unified_sgo", all_go_terms=unified_sgo_terms)
-
-# subprocess.call(shlex.split("../tools/owltools ../tools/go.obo --gaf ../data-sources/yeast/sgd.gaf --map2slim --idfile ../tmp/unified_sgo_terms --write-gaf ../tmp/yeast.unified.sgo.gaf"))
-# feature_preprocessing.sgo.main("../generated-data/ppc_yeast", "../tmp/yeast.unified.sgo.gaf", 2, output_path="../generated-data/features/ppc_yeast_unified_sgo", all_go_terms=unified_sgo_terms, annotations_reader=feature_preprocessing.sgo.read_annotations_yeast)
-
 
 # 1. Train yeast refined model on full yeast dataset and evaluate on the other three
 
@@ -51,6 +26,7 @@ refined_cfg_path = "cfgs/models/yeast_smf_refined_model.json"
 with open(refined_cfg_path, 'r') as f:
     refined_cfg = json.load(f)
 refined_cfg['train_on_full_dataset'] = True 
+refined_cfg['epochs'] = 10000
 refined_cfg['train_model'] = True 
 refined_cfg['spec'][1]['paths'] = ['../generated-data/features/ppc_yeast_%s.npz' % go_postfix]
 os.makedirs('%s/refined_model' % output_dir, exist_ok=True)
@@ -187,7 +163,7 @@ cfg['test_on_full_dataset'] = True
 cfg["trained_model_path"] = refined_cfg["trained_model_path"]
 models.null_model.main(cfg, 0, 0, "%s/dro_null" % output_dir)
 
-# # Analysis
-# analysis.fig_cv_performance.main("cfgs/fig_cv_performance/generalization_smf_pombe.json")
-# analysis.fig_cv_performance.main("cfgs/fig_cv_performance/generalization_smf_human.json")
-# analysis.fig_cv_performance.main("cfgs/fig_cv_performance/generalization_smf_dro.json")
+# Analysis
+analysis.fig_cv_performance.main("cfgs/fig_cv_performance/generalization_smf_pombe.json")
+analysis.fig_cv_performance.main("cfgs/fig_cv_performance/generalization_smf_human.json")
+analysis.fig_cv_performance.main("cfgs/fig_cv_performance/generalization_smf_dro.json")
