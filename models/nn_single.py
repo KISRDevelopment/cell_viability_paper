02db0810dc385e3265878d386f397bc9a70ce2ea
Mohammad Khajah <mmkhajah@gmail.com>
2022-04-19 20:39:50 +0300

    more work on nn_single

--- models/nn_single.py
+++ models/nn_single.py
@@ -3,40 +3,60 @@ import tensorflow.keras as keras
 import numpy as np
 import json 
 import pandas as pd 
+
 def train_model(model_spec, train_df, valid_df):
-    if len(model_spec['selected_feature_sets']) == 0:
-        model_spec['selected_feature_sets'] = list(model_spec['feature_sets'].keys())
     
-    df_cols = train_df.columns 
-    
-    for feature_set, props in model_spec['feature_sets'].items():
-        ix = df_cols.str.startswith('%s-' % feature_set)
-        props['dim'] = np.sum(ix)
-        props['cols'] = df_cols[ix]
-
-
-    train_Y = keras.utils.to_categorical(train_df[model_spec['target_col']])
-    valid_Y = keras.utils.to_categorical(valid_df[model_spec['target_col']])
-
-    model = create_model(model_spec, train_Y.shape[1])
+    model = create_model(model_spec, model_spec['n_output_dim'])
 
     train_inputs = create_inputs(model_spec, train_df)
-    model.fit(train_inputs, train_Y, epochs=100, verbose=True)
+    train_inputs, mus, stds = normalize_inputs(model_spec, train_inputs)
+    train_Y = keras.utils.to_categorical(train_df[model_spec['target_col']])
+    
+    valid_inputs = create_inputs(model_spec, valid_df)
+    valid_inputs, _, _ = normalize_inputs(model_spec, valid_inputs, mus, stds)
+    valid_Y = keras.utils.to_categorical(valid_df[model_spec['target_col']])
+
+    earlystopping_callback = keras.callbacks.EarlyStopping(monitor='val_loss', 
+                                patience=model_spec['patience'], restore_best_weights=True)
+    callbacks = [earlystopping_callback]
+
+    model.fit(train_inputs, 
+              train_Y, 
+              batch_size=int(model_spec['batch_size_p'] * train_Y.shape[0]),
+              epochs=model_spec['epochs'],
+              verbose=model_spec['verbose'],
+              validation_data=(valid_inputs, valid_Y),
+              validation_batch_size=100000,
+              callbacks=callbacks)
+
+    return model, mus, stds
+
 
 def create_inputs(model_spec, df):
 
     inputs = []
-    for feature_set, props in model_spec['feature_sets'].items():
+    for feature_set in model_spec['selected_feature_sets']:
+        props = model_spec['feature_sets'][feature_set]
         F = np.array(df[ props['cols'] ])
-        
-        if props['normalize']:
-            mu = np.mean(F, axis=0)
-            std = np.std(F, axis=0, ddof=1)
-            F = (F - mu) / std 
-        
         inputs.append(F)
     
     return inputs
+
+def normalize_inputs(model_spec, inputs, mus = None, stds = None):
+
+    if mus is None:
+        mus = [np.mean(F, axis=0) for F in inputs]
+        stds = [np.std(F, axis=0, ddof=1)+1e-9 for F in inputs]
+    
+    normalized_inputs = []
+    for feature_set, F, mu, std in zip(model_spec['selected_feature_sets'], inputs, mus, stds):
+        props = model_spec['feature_sets'][feature_set]
+
+        if props['normalize']:
+            F = (F - mu) / std
+        normalized_inputs.append(F)
+        
+    return normalized_inputs, mus, stds
 
 def create_model(model_spec, output_dim):
     selected_feature_sets = model_spec['selected_feature_sets']
@@ -114,12 +134,11 @@ def weighted_categorical_xentropy(y_true, y_pred):
 def main(spec_path, dataset_path, splits_path, rep):
     rep = int(rep)
 
-    with open(spec_path, 'r') as f:
-        model_spec = json.load(f)
-
     df = pd.read_csv(dataset_path)
 
-    split = get_split(splits_path, rep)
+    model_spec = load_spec(spec_path, df)
+
+    split = load_split(splits_path, rep)
 
     train_ix = split == 1
     valid_ix = split == 2
@@ -129,14 +148,38 @@ def main(spec_path, dataset_path, splits_path, rep):
     valid_df = df[valid_ix]
     test_df = df[test_ix]
 
-    train_model(model_spec, train_df, test_df)
+    model, mus, stds = train_model(model_spec, train_df, valid_df)
 
-def get_split(splits_path, rep):
+    test_inputs = create_inputs(model_spec, test_df)
+    test_inputs, _, _ = normalize_inputs(model_spec, test_inputs, mus, stds)
+    preds = model.predict(test_inputs, batch_size=1000000)
+
+    print(preds.shape)
+def load_split(splits_path, rep):
 
     d = np.load(splits_path)
     splits = d['splits']
     return splits[rep,:]
+
+def load_spec(spec_path, df):
+
+    with open(spec_path, 'r') as f:
+        model_spec = json.load(f)
+
+    if len(model_spec['selected_feature_sets']) == 0:
+        model_spec['selected_feature_sets'] = list(model_spec['feature_sets'].keys())
     
+    # add the feature dimensions and column names for each feature set
+    for feature_set, props in model_spec['feature_sets'].items():
+        ix = df.columns.str.startswith('%s-' % feature_set)
+        props['dim'] = np.sum(ix)
+        props['cols'] = list(df.columns[ix])
+
+    # calculate output dimension size (number of classes)
+    model_spec['n_output_dim'] = np.unique(df[model_spec['target_col']]).shape[0]
+    
+    return model_spec
+
 if __name__ == "__main__":
     import sys 
     main(*sys.argv[1:])
