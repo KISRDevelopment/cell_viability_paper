9c4f5fa70d9b9bf01766bf405019a825c8bb8a81
Mohammad Khajah <mmkhajah@gmail.com>
2022-05-03 23:48:06 +0300

    remove junk

--- table1_smf_fs.py
+++
@@ -1,9 +0,0 @@
-#
-# Make Supplementary Table 1: Feature Selection Results
-#
-import numpy as np
-import pandas as pd
-import analysis.tbl_model_comp
-
-analysis.tbl_model_comp.main("../results/yeast_smf_30_fs", "../tables/table1.xlsx", analysis.tbl_model_comp.SMF_LABELS)
-
--- table3_gi_fs.py
+++
@@ -1,9 +0,0 @@
-#
-# Make Supplementary Table 1: Feature Selection Results
-#
-import numpy as np
-import pandas as pd
-import analysis.tbl_model_comp
-
-analysis.tbl_model_comp.main("../results/task_yeast_gi_hybrid_fs", 
-     "../tables/table3.xlsx", analysis.tbl_model_comp.GI_LABELS)
